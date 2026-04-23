4ce95b50394a52e6d5ad17bfba627aa0f69f2883
Hafeez <hapiz91@gmail.com>
2026-04-23 11:59:07 +0400

    fix port for render

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 app.use(express.json());
 app.use(express.static("public"));
